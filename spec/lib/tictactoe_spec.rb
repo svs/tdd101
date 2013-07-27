2ac61db8317d0a213de9cb2ae585e0f28b90998a
svs <svs@svs.io>
2013-07-27 16:03:50 +0530

    updated spec file

--- spec/lib/tictactoe_spec.rb
+++ spec/lib/tictactoe_spec.rb
@@ -3,79 +3,57 @@ require './lib/tictactoe.rb'
 
 describe TicTacToe do
 
-  let(:new_game) { TicTacToe.new }
-  let(:game_with_one_player) { new_game.tap{|g| g.add_player("foo") } }
-  let(:game_with_two_players) { game_with_one_player.tap{|g| g.add_player("bar") } }
-  
-  context "with a brand new game" do
-    subject { new_game }
-    
+  context "at the start" do
+    let(:game) { TicTacToe.new }
+    let(:game_with_one_player) { game.tap{|g| g.add_player("foo") } }
+    let(:game_with_two_players) { game_with_one_player.tap{|g| g.add_player("bar") } }
+    subject { game }
+
+    its(:moves) { should be_empty }
     its(:players) { should be_empty }
-    it { should_not be_startable }
+
     it { should_not be_started }
     it { should_not be_finished }
+    it { should_not be_won }
     its(:winner) { should be_nil }
-    it { should respond_to :add_player}
-  end
-
-  context "with one player" do
-
-    subject { game_with_one_player } 
-
-    its(:players) { should == ["foo"] }
-    it { should_not be_startable }
-    it { should_not be_started }
-    it { should_not be_finished }
-    its(:winner) { should be_nil }
-
-  end
-
-  context "with two player" do
-
-    subject { game_with_two_players }
-
-    its(:players) { should == ["foo", "bar"] }
-    it { should be_startable }
-    it { should be_started }
-    its(:current_player) { should == "foo" }
-    it { should_not be_finished }
-    its(:winner) { should be_nil }
-
-  end
-
-  context "started game" do
-    subject { game_with_two_players }
-
-    before(:each) { subject.add_move(1,1) }
-    its(:current_player) { should == "bar" }
-    it { should be_started }
-    it { should_not be_finished }
-    its(:winner) { should be_nil }
-
-    it "should not add the same move twice" do
-      expect { subject.add_move(1,1) }.to raise_error
+    its(:current_player) { should be_nil }
+    it "should not accept moves" do
+      expect { game.add_move(1,1) }.to raise_error(ArgumentError)
     end
-    
-    it "should limit moves to the extent of the board" do
-      expect { subject.add_move(3,3) }.to raise_error
-      expect { subject.add_move(-1,-1) }.to raise_error
+
+    describe "game with one player" do
+      subject { game_with_one_player }
+      its(:players) { should == ["foo"] }
+      it { should_not be_started }
+      it { should_not be_finished }
+      it { should_not be_won }
+      its(:winner) { should be_nil }
+      its(:current_player) { should be_nil }
+      it "should not accept moves" do
+        expect { game.add_move(1,1) }.to raise_error(ArgumentError)
+      end
+
     end
+
+    describe "game with two players" do
+      subject { game_with_two_players }
+      its(:players) { should == ["foo","bar"] }
+      it "should not allow adding a third player" do
+        expect { game_with_two_players.add_player("baz") }.to raise_error
+      end
+      it { should be_started }
+      it { should_not be_finished }
+      it { should_not be_won }
+      its(:winner) { should be_nil }
+      its(:current_player) { should == "foo" }
+      specify { expect { subject.add_move(1,1) }.to_not raise_error }
+      it "should add moves" do
+        subject.add_move(1,1)
+        #subject.moves.should ==
+      end
+
+
+    end
+
   end
-
-  context "finished game" do
-    subject { game_with_two_players.tap{|g|
-
-        [[1,1],[0,0],[0,1],[1,0],[2,1]].each do |x,y|
-          g.add_move(x,y)
-        end
-      }
-    }
-
-    it { should be_finished }
-    its(:winner) { should == "foo" }
-
-  end
-
-
 end
-
